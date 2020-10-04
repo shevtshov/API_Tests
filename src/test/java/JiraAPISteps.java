e4cde535ee1121fcaddd65165bb81aa278293272
vs <v.shevtsov@comparus.de>
2020-10-04 19:44:33 +0300

    added yaml file

--- src/test/java/JiraAPISteps.java
+++ src/test/java/JiraAPISteps.java
@@ -3,11 +3,11 @@ import io.restassured.response.Response;
 public class JiraAPISteps {
 
 
-    public static Response createIssue() {
-        String issueJSON = JiraJSONObjects.newIssueJSON();
-        Response response = HTTPMethods.post(APIPathes.issue, issueJSON);
-        return response;
-    }
+//    public static Response createIssue() {
+//        String issueJSON = JiraJSONObjects.newIssueJSON();
+//        Response response = HTTPMethods.post(APIPathes.issue, issueJSON);
+//        return response;
+//    }
 }
 
 
